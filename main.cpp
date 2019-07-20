b18d79d8ecc34636c7c8b031f6c8509482aa0e62
Anton Tcholakov <anton.pt@outlook.com>
2019-07-20 22:36:54 +0100

    Implement tagged_untyped and tagged<T> with hashing and equality comparison

--- main.cpp
+++ main.cpp
@@ -3,15 +3,59 @@
 //
 
 #include <iostream>
+#include "immer/set.hpp"
+
 
 #include "atom.h"
+#include "tagged.h"
+
+const aeternum::atom apples("apples");
+const aeternum::atom oranges("oranges");
+
+int fruit_count(const aeternum::tagged_untyped& tagged)
+{
+    if (auto as = tagged.match<apples, int>())
+    {
+        return *as;
+    }
+    if (auto os = tagged.match<oranges, int>())
+    {
+        return *os;
+    }
+
+    return 0;
+}
 
 int main()
 {
-    const aeternum::atom hello("hello");
-    const aeternum::atom goodbye("goodbye");
+    const auto four_apples = aeternum::make_tagged(apples, 4);
+    const auto four_oranges = aeternum::make_tagged(oranges, 4);
+    const auto five_apples = aeternum::make_tagged(apples, 5);
+    const auto four_more_oranges = aeternum::make_tagged(oranges, 4);
 
-    std::cout << "Comparing two different atoms: " << (hello == goodbye) << std::endl;
+    std::cout << "The hash of 4 apples is: " << std::hash<aeternum::tagged_untyped>{}(four_apples) << std::endl;
+    std::cout << "The hash of 4 oranges is: " << std::hash<aeternum::tagged_untyped>{}(four_oranges) << std::endl;
+    std::cout << "The hash of 5 apples is: " << std::hash<aeternum::tagged_untyped>{}(five_apples) << std::endl;
+    std::cout << "The hash of 4 more oranges is: " << std::hash<aeternum::tagged_untyped>{}(four_more_oranges) << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Comparing 4 apples and 4 oranges: " << (four_apples == four_oranges) << std::endl;
+    std::cout << "Comparing 4 apples and 5 apples: " << (four_apples == five_apples) << std::endl;
+    std::cout << "Comparing 4 oranges and 4 more oranges: " << (four_oranges == four_more_oranges) << std::endl;
+
+    std::cout << std::endl;
+
+    immer::set<aeternum::tagged_untyped> fruit;
+    fruit = fruit.insert(four_apples);
+    fruit = fruit.insert(four_oranges);
+    fruit = fruit.insert(five_apples);
+    fruit = fruit.insert(four_more_oranges);
+
+    for (auto& f : fruit)
+    {
+        std::cout << "Set contains " << fruit_count(f) << " " << f.get_tag().name << std::endl;
+    }
 
     return 0;
 }
---
+++ tagged.h
@@ -0,0 +1,138 @@
+//
+// Created by Anton Tcholakov on 2019-07-20.
+//
+
+#pragma once
+
+#include <functional>
+#include <memory>
+#include <set>
+
+#include "atom.h"
+
+namespace aeternum {
+
+    template<typename T>
+    class tagged;
+
+    class tagged_untyped {
+    public:
+
+        virtual ~tagged_untyped() = default;
+
+        template<typename T, typename Hash = std::hash<T>, typename Equals = std::equal_to<T>>
+        tagged_untyped(atom tag, std::shared_ptr<T> data);
+
+        tagged_untyped(const tagged_untyped& other) = default;
+        tagged_untyped(tagged_untyped&& other) noexcept = default;
+
+        tagged_untyped& operator=(const tagged_untyped& other) = default;
+        tagged_untyped& operator=(tagged_untyped&& other) = default;
+
+        template<const atom& tag, typename T>
+        const tagged<T> match() const;
+
+        const atom get_tag() const { return _tag; }
+
+        operator bool() { return _data != nullptr; }
+
+        std::size_t get_hash() const;
+
+        bool operator==(const tagged_untyped& rhs) const {
+            return get_tag() == rhs.get_tag()
+                   && _equality_comparer(_data, rhs._data);
+        }
+
+    protected:
+        atom _tag;
+        std::shared_ptr<void> _data;
+        std::function<std::size_t(const std::shared_ptr<void>&)> _hasher;
+        std::function<bool(const std::shared_ptr<void>&, const std::shared_ptr<void>&)> _equality_comparer;
+    };
+
+    template<typename T, typename Hash, typename Equals>
+    tagged_untyped::tagged_untyped(atom tag, std::shared_ptr<T> data)
+            : _tag(tag),
+              _data(std::static_pointer_cast<void>(data)),
+              _hasher([](const std::shared_ptr<void>& data) { return Hash{}(*(std::static_pointer_cast<T>(data))); }),
+              _equality_comparer([](const std::shared_ptr<void>& lhs, const std::shared_ptr<void>& rhs) { return lhs == rhs ? true : Equals{}(*std::static_pointer_cast<T>(lhs), *std::static_pointer_cast<T>(rhs)); }) { }
+
+    template<typename T>
+    class tagged : public tagged_untyped {
+    public:
+        tagged(atom tag, std::shared_ptr<T> data);
+
+        tagged(const tagged& other) = default;
+        tagged(tagged&& other) noexcept = default;
+
+        tagged& operator=(const tagged& other) = default;
+        tagged& operator=(tagged&& other) noexcept = default;
+
+        T& operator*() const;
+        T* operator->() const;
+    };
+
+    template<const atom& tag, typename T>
+    const tagged<T> tagged_untyped::match() const {
+        return _tag == tag
+               ? tagged<T>(tag, std::static_pointer_cast<T>(_data))
+               : tagged<T>(tag, nullptr);
+    }
+
+    std::size_t tagged_untyped::get_hash() const {
+        std::size_t h1 = std::hash<aeternum::atom>{}(get_tag());
+        std::size_t h2 = _hasher(_data);
+        return h1 ^ (h2 << 1);
+    }
+
+    template<typename T>
+    tagged<T>::tagged(atom tag, std::shared_ptr<T> data) : tagged_untyped(tag, data) { }
+
+    template<typename T>
+    T& tagged<T>::operator*() const {
+        return *(std::static_pointer_cast<T>(_data));
+    }
+
+    template<typename T>
+    T* tagged<T>::operator->() const {
+        return std::static_pointer_cast<T>(_data).operator->();
+    }
+
+    template<typename T>
+    tagged<T> make_tagged(atom tag, T&& data) {
+        return tagged<T>(tag, std::make_shared<T>(data));
+    }
+
+    template<typename T, class Compare = std::less<T>>
+    bool operator<(const tagged<T>& lhs, const tagged<T>& rhs)
+    {
+        return lhs.get_tag() < rhs.get_tag()
+               || (lhs.get_tag() == rhs.get_tag() && Compare(*lhs, *rhs));
+    }
+}
+
+namespace std {
+    template<>
+    struct hash<aeternum::tagged_untyped>
+    {
+        typedef aeternum::tagged_untyped argument_type;
+        typedef std::size_t result_type;
+
+        result_type operator()(const argument_type& tagged) const noexcept
+        {
+            return tagged.get_hash();
+        }
+    };
+
+    template<typename T>
+    struct hash<aeternum::tagged<T>>
+    {
+        typedef aeternum::tagged<T> argument_type;
+        typedef std::size_t result_type;
+
+        result_type operator()(const argument_type& tagged) const noexcept
+        {
+            return tagged.get_hash();
+        }
+    };
+}
